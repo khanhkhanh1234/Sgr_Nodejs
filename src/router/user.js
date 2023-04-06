8edebbd9e654a15589c3ca85aef2782ac3687bd1
khanhkhanh1234 <nguyenkimt322@gmail.com>
2023-04-06 21:12:47 +0700

    validate

--- src/router/user.js
+++ src/router/user.js
@@ -19,14 +19,17 @@ user_router.get('/', function (req, res) {
     res.json(object);
 })
 user_router.get('/:id', function (req, res) {
+    var user = req.params.id;
     res.json(object[req.params.id]);  
 })
-user_router.post('/', function (req, res) {
+user_router.post('/', validateUser, function (req, res) {
+    var user = req.body;
+    console.log(req.body);
     req.body.id = ++count;
     object.push(req.body);
     res.status(201).json(object);
 })
-user_router.put('/:id', function (req, res) {
+user_router.put('/:id', validateUser,function (req, res) {
     object[req.params.id] = req.body;
     req.body.id = object.length-1;
     res.status(204).json(object);
@@ -38,4 +41,15 @@ user_router.delete('/:id', function (req, res) {
     }
     res.status(204).json(object);
 })
+function validateUser(req, res, next) {
+    var user = req.body;
+    if (/[^a-zA-Z\s]/.test(user.fullname) || user.fullname.length < 6 ||user.age <= 0) {
+        res.status(400).json({ message: "fullname is invalid" });
+        return;
+    }
+    else {
+        next();
+    }
+    
+}
 module.exports = user_router;
